5e5f1e937cb0a2f14f574c3be08272eeb0235fa3
“Aleksandr <ren_alex@mail.ru>
2020-09-02 23:57:20 +0400

    add gulp-inject-string

--- gulpfile.js
+++ gulpfile.js
@@ -26,7 +26,7 @@ let path = {
     js: source_folder + "/js/**/*.js",
     img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
   },
-  clean: [project_folder , project_app_folder],
+  clean: [project_folder, project_app_folder],
 };
 
 const { src, dest } = require('gulp'),
@@ -34,14 +34,15 @@ const { src, dest } = require('gulp'),
   browsersync = require('browser-sync').create(),
   watch = require('gulp-watch'),
   sass = require('gulp-sass'),
-  autoprefixer = require('gulp-autoprefixer'), 
+  autoprefixer = require('gulp-autoprefixer'),
   sourcemaps = require('gulp-sourcemaps'),
   notify = require('gulp-notify'),
   plumber = require('gulp-plumber'),
   fileinclude = require('gulp-file-include'),
   del = require("del"),
   favicons = require("favicons").stream,
-  log = require("fancy-log");
+  log = require("fancy-log"),
+  inject = require("gulp-inject-string");
 
 
 
@@ -66,13 +67,13 @@ function html() {
 
     .pipe(fileinclude({ prefix: '@@' }))
     .pipe(dest(path.build.html))
-    .pipe(dest('app/'))
-    .pipe(browsersync.stream());
+    .pipe(dest(path.build.app_html))
+    .pipe(browsersync.stream())
+    .pipe(gulp.dest(path.src.html));
 };
 
 
-
-function css () {
+function css() {
   return src(path.src.css)
     .pipe(
       sass({
@@ -91,8 +92,8 @@ function css () {
     .pipe(sourcemaps.init())
     .pipe(sass())
     .pipe(autoprefixer({
-        overrideBrowserslist: ["last 4 version"]
-      })
+      overrideBrowserslist: ["last 4 version"]
+    })
     )
     .pipe(dest(path.build.css))
     .pipe(sourcemaps.write())
@@ -128,7 +129,7 @@ gulp.task("delfavicon", function () {
 });
 
 gulp.task("favicon", function () {
-  return gulp.src([source_folder + '/img/favicon.png'])
+  return gulp.src([source_folder + '/img/favicon.{png, jpg, svg}'])
     .pipe(favicons({
       path: "/img/favicon/",                                // Path for overriding default icons path. `string`
       appName: 'CodeTime',                            // Your application's name. `string`
@@ -179,6 +180,59 @@ gulp.task("favicon", function () {
 });
 
 
+gulp.task('faviconAddMeta', function (callback) {
+  gulp.src(path.src)
+    .pipe(inject.beforeEach('</head>', '<link rel="shortcut icon" href="/img/favicon/favicon.ico">' +
+      '            <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon/favicon-16x16.png">' +
+      '        <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon/favicon-32x32.png">' +
+      '        <link rel="icon" type="image/png" sizes="48x48" href="/img/favicon/favicon-48x48.png">' +
+      '        <link rel="manifest" href="/img/favicon/manifest.json">' +
+      '        <meta name="mobile-web-app-capable" content="yes">' +
+      '        <meta name="theme-color" content="#fff">' +
+      '        <meta name="application-name" content="CodeTime">' +
+      '        <link rel="apple-touch-icon" sizes="57x57" href="/img/favicon/apple-touch-icon-57x57.png">' +
+      '        <link rel="apple-touch-icon" sizes="60x60" href="/img/favicon/apple-touch-icon-60x60.png">' +
+      '        <link rel="apple-touch-icon" sizes="72x72" href="/img/favicon/apple-touch-icon-72x72.png">' +
+      '        <link rel="apple-touch-icon" sizes="76x76" href="/img/favicon/apple-touch-icon-76x76.png">' +
+      '        <link rel="apple-touch-icon" sizes="114x114" href="/img/favicon/apple-touch-icon-114x114.png">' +
+      '        <link rel="apple-touch-icon" sizes="120x120" href="/img/favicon/apple-touch-icon-120x120.png">' +
+      '        <link rel="apple-touch-icon" sizes="144x144" href="/img/favicon/apple-touch-icon-144x144.png">' +
+      '        <link rel="apple-touch-icon" sizes="152x152" href="/img/favicon/apple-touch-icon-152x152.png">' +
+      '        <link rel="apple-touch-icon" sizes="167x167" href="/img/favicon/apple-touch-icon-167x167.png">' +
+      '        <link rel="apple-touch-icon" sizes="180x180" href="/img/favicon/apple-touch-icon-180x180.png">' +
+      '        <link rel="apple-touch-icon" sizes="1024x1024" href="/img/favicon/apple-touch-icon-1024x1024.png">' +
+      '        <meta name="apple-mobile-web-app-capable" content="yes">' +
+      '        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">' +
+      '        <meta name="apple-mobile-web-app-title" content="CodeTime">' +
+      '        <link rel="apple-touch-startup-image" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"    href="/img/favicon/apple-touch-startup-image-640x1136.png">' +
+      '        <link rel="apple-touch-startup-image" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"    href="/img/favicon/apple-touch-startup-image-750x1334.png">' +
+      '        <link rel="apple-touch-startup-image" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"    href="/img/favicon/apple-touch-startup-image-828x1792.png">' +
+      '        <link rel="apple-touch-startup-image" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"    href="/img/favicon/apple-touch-startup-image-1125x2436.png">' +
+      '        <link rel="apple-touch-startup-image" media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"    href="/img/favicon/apple-touch-startup-image-1242x2208.png">' +
+      '        <link rel="apple-touch-startup-image" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"    href="/img/favicon/apple-touch-startup-image-1242x2688.png">' +
+      '        <link rel="apple-touch-startup-image" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"   href="/img/favicon/apple-touch-startup-image-1536x2048.png">' +
+      '        <link rel="apple-touch-startup-image" media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"   href="/img/favicon/apple-touch-startup-image-1668x2224.png">' +
+      '        <link rel="apple-touch-startup-image" media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"   href="/img/favicon/apple-touch-startup-image-1668x2388.png">' +
+      '        <link rel="apple-touch-startup-image" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"  href="/img/favicon/apple-touch-startup-image-2048x2732.png">' +
+      '        <link rel="apple-touch-startup-image" media="(device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"  href="/img/favicon/apple-touch-startup-image-1620x2160.png">' +
+      '    <link rel="apple-touch-startup-image" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"   href="/img/favicon/apple-touch-startup-image-1136x640.png">' +
+      '        <link rel="apple-touch-startup-image" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"   href="/img/favicon/apple-touch-startup-image-1334x750.png">' +
+      '        <link rel="apple-touch-startup-image" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"   href="/img/favicon/apple-touch-startup-image-1792x828.png">' +
+      '        <link rel="apple-touch-startup-image" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"   href="/img/favicon/apple-touch-startup-image-2436x1125.png">' +
+      '        <link rel="apple-touch-startup-image" media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"   href="/img/favicon/apple-touch-startup-image-2208x1242.png">' +
+      '        <link rel="apple-touch-startup-image" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"   href="/img/favicon/apple-touch-startup-image-2688x1242.png">' +
+      '        <link rel="apple-touch-startup-image" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"  href="/img/favicon/apple-touch-startup-image-2048x1536.png">' +
+      '        <link rel="apple-touch-startup-image" media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"  href="/img/favicon/apple-touch-startup-image-2224x1668.png">' +
+      '        <link rel="apple-touch-startup-image" media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"  href="/img/favicon/apple-touch-startup-image-2388x1668.png">' +
+      '        <link rel="apple-touch-startup-image" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)" href="/img/favicon/apple-touch-startup-image-2732x2048.png">' +
+      '        <link rel="apple-touch-startup-image" media="(device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"  href="/img/favicon/apple-touch-startup-image-2160x1620.png">' +
+      '        <link rel="icon" type="image/png" sizes="228x228" href="/img/favicon/coast-228x228.png">' +
+      '        <meta name="msapplication-TileColor" content="#fff">' +
+      '        <meta name="msapplication-TileImage" content="/img/favicon/mstile-144x144.png">' +
+      '        <meta name="msapplication-config" content="/img/favicon/browserconfig.xml">' +
+      '        <link rel="yandex-tableau-widget" href="/img/favicon/yandex-browser-manifest.json"></link>')),
+    callback();
+});
 
 const clean = () => del(path.clean);
 
@@ -189,7 +243,7 @@ let watching = gulp.parallel(build, browserSync, watchFiles);
 // exports.delfavicon = delfavicon;
 // exports.favicon = favicon;
 // exports.watchFiles = watchFiles;
-exports.clean = clean;
+// exports.index = index;
 exports.images = images;
 exports.js = js;
 exports.css = css;
@@ -197,3 +251,4 @@ exports.html = html;
 exports.build = build;
 exports.watching = watching;
 exports.default = watching;
+exports.faviconAddMeta = faviconAddMeta;
